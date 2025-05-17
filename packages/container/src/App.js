638e99d1277e6fb31b2aa2ff84c00a8b9cb8d20d
sujitoic999 <sujitfgiet@gmail.com>
2025-05-17 15:13:54 +0530

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { MarketingReactComponent } from "marketing/MarketingApp";
+import MarketingApp from "./components/MarketingApp";
+// console.log(mount);
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
